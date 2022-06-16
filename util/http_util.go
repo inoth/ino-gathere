2a06fb74a3a184d2e1e613dc23344d3b77e0fcc6
inoth <inoth.work@gmail.com>
2022-06-16 18:34:15 +0800

    .

--- util/http_util.go
+++ util/http_util.go
@@ -1,5 +1,5 @@
 package util
 
-func Post(url string, body interface{}) error {
-	return nil
-}
+func Post(url string, body interface{}) error { return nil }
+
+func Get(url, query interface{}) error { return nil }
